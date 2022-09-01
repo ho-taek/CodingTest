d0de525d8706a81bc032f0c9c692d1800b7267cb
ho-taek <kht072623@naver.com>
2022-09-01 13:59:30 +0900

    Create PG-모음사전(LEVEL2).py

---
+++ Brute force/PG-모음사전(LEVEL2).py
@@ -0,0 +1,11 @@
+from itertools import product
+
+def solution(word):
+    words = []
+    
+    for i in range(1,6):
+        for c in product(['A','E','I','O','U'], repeat = i):
+            words.append(''.join(list(c)))
+        
+    words.sort()
+    return words.index(word)+1
