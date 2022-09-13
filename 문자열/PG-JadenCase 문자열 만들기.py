51096000de23fa0749df332d050a1530f0bcb84d
ho-taek <kht072623@naver.com>
2022-09-13 21:24:17 +0900

    Create PG-JadenCase 문자열 만들기.py

---
+++ 문자열/PG-JadenCase 문자열 만들기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = s[0].upper() 
+    for i in range(1, len(s)):
+        if s[i-1] == " " and s[i] != " ":
+            answer += s[i].upper() 
+        else:
+            answer += s[i].lower()
+    return answer
