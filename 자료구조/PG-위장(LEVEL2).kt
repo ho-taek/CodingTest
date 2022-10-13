c0a68c5d4e832105fcbdd6fb2f9e698d43a3c929
ho-taek <kht072623@naver.com>
2022-10-13 16:30:56 +0900

    Delete PG-위장(LEVEL2).kt

--- 자료구조/PG-위장(LEVEL2).kt
+++
@@ -1,7 +0,0 @@
-class Solution {
-    fun solution(clothes: Array<Array<String>>): Int {
-        val hash = clothes.groupBy{it[1]}.values
-        .fold(1){total, i -> total * (i.size+1)} - 1
-        return hash
-    }
-}
