fc6e53cd2fe76d9d81a33c3d8a63a040894fc860
ho-taek <kht072623@naver.com>
2022-10-14 15:11:03 +0900

    Update 1439-뒤집기_실5.kt

--- greedy/1439-뒤집기_실5.kt
+++ greedy/1439-뒤집기_실5.kt
@@ -8,20 +8,25 @@ fun main(args: Array<String>){
 fun result(s : String) : Int{
     var one = 0
     var zero = 0
-    stack = mutableListOf(s[0])
-    if s[0] == "0" one++ else zero ++
-
-    if s.length == s.count {c -> c == '1' or c == '0'} return 0
+    val stack = mutableListOf(s[0])
+    if (s[0] == "0"){
+        one++
+    }  else {
+        zero ++
+    }
+    if (s.length == s.count {c -> c == '1'}){
+        return 0
+    } 
 
     for(i in 1..s.length){
-        if s[i] == "1" and stack[0] == "0"{
+        if (s[i] == "1" and stack[0] == "0"){
             stack.removeAt(0)
             one++
             stack.add("1")
         }
             
         
-        if s[i] == "0" and stack[0] == "1"{
+        if (s[i] == "0" and stack[0] == "1"){
             stack.removeAt(0)
             zero++
             stack.add("0")
@@ -29,5 +34,5 @@ fun result(s : String) : Int{
 
     }
 
-    return min(one,zero)
+    return one
 }
