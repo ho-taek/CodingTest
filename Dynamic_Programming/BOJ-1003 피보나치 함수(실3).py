94525ca1fcc211d6ff254c2d0fcec92473ef5195
ho-taek <kht072623@naver.com>
2022-12-19 15:43:12 +0900

    Create BOJ-1003 피보나치 함수(실3).py

---
+++ Dynamic_Programming/BOJ-1003 피보나치 함수(실3).py
@@ -0,0 +1,28 @@
+import sys
+read = sys.stdin.readline
+
+t = int(read())
+m = 0
+cache = [[0,0] for i in range(41)]
+cache[0]=[1,0]
+cache[1]=[0,1]
+def fib(n):
+    if cache[n] != [0,0]:
+        return cache[n]
+    for i in range(2,n+1):
+            cache[i][0] = cache[i-2][0] + cache[i-1][0]
+            cache[i][1] = cache[i-2][1] + cache[i-1][1]
+    return cache[n]
+
+for _ in range(t):
+    n = int(read())
+    answer = fib(n)
+    print('{} {}'.format(answer[0],answer[1]))
+
+
+
+
+    
+
+
+    
