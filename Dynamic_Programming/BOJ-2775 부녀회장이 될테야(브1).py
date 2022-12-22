b233b09d26bcb3277161f68873f9a5b4ef14f518
ho-taek <kht072623@naver.com>
2022-12-22 23:12:45 +0900

    Create BOJ-2775 부녀회장이 될테야(브1).py

---
+++ Dynamic_Programming/BOJ-2775 부녀회장이 될테야(브1).py
@@ -0,0 +1,18 @@
+import sys
+read= sys.stdin.readline
+
+t = int(read())
+
+
+answer = []
+for _ in range(t):
+    graph = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    k = int(read())
+    n = int(read())
+
+    if n == 1:
+        print(1)
+    for i in range(1,k+1):
+        for j in range(1,n+1):
+            graph[j] = graph[j-1]+graph[j]
+    print(graph[n])
