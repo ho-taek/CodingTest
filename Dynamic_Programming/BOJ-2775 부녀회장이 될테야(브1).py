5cb8fe85c688e3c688b98b5b8edf2cca403c0191
ho-taek <kht072623@naver.com>
2022-12-26 20:36:10 +0900

    Update BOJ-2775 부녀회장이 될테야(브1).py

--- Dynamic_Programming/BOJ-2775 부녀회장이 될테야(브1).py
+++ Dynamic_Programming/BOJ-2775 부녀회장이 될테야(브1).py
@@ -12,7 +12,8 @@ for _ in range(t):
 
     if n == 1:
         print(1)
+        continue
     for i in range(1,k+1):
         for j in range(1,n+1):
-            graph[j] = graph[j-1]+graph[j]
+            graph[j] += graph[j-1]
     print(graph[n])
