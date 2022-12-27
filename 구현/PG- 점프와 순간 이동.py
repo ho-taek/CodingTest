b03db1c499a8cfad27d2ae53173a1933d1bcfeef
ho-taek <kht072623@naver.com>
2022-12-27 18:11:04 +0900

    Update PG- 점프와 순간 이동.py

--- 구현/PG- 점프와 순간 이동.py
+++ 구현/PG- 점프와 순간 이동.py
@@ -2,12 +2,13 @@ def solution(n):
     ans = 0
     
     while True:
-        if n == 0:
-            break
-        if n%2 != 0:
+        if n == 1:
             ans += 1
-            n -= 1
-        else:
+            break
+        if n % 2 == 0:
             n /= 2
+        else:
+            n -= 1
+            ans += 1
 
     return ans
